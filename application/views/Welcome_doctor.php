61fef025b8b3571589116437e10542bb31a16f75
Pilonetto <marlon.pilonetto@gmail.com>
2017-11-17 14:02:25 -0200

    clica

--- application/views/Welcome_doctor.php
+++ application/views/Welcome_doctor.php
@@ -117,12 +117,73 @@
 								</div>
 							</div>																				 	
 						<!----calendar- -->
+						<!----new start- -->
 						    <div id="menus1" class="tab-pane fade">
-								<div class="tab-cnt-search">
-								   <h6> Agendamento </h6>
+								<div class="container">
+									<div class="doctor-sub">
+										<h3><img src="<?php echo base_url(); ?>assets/images/patient-login/12.png"  ></h3>
+										<h4><?php if($this->lang->line('doctorfilter_slide_A13')){ ?><?php echo $this->lang->line('doctorfilter_slide_A13'); }else{ ?>Select a Speciality Doctor <?php } ?></h4>
+									</div>
+									<div class="doctor-pat-srch">     
+										<div class="row">
+											<input type="hidden" value='<?php echo $actual_data;?>' id="actual_data">
+											<div class="evnt-mn doctor" id="updates">
+												<?php if(!empty($doctors)) { 
+														if(isset($doctors)) { ?>
+												<div class="col-lg-6" style="padding-right: 0px;">
+												<?php foreach($doctors as $doctor_detail){ ?>
+													<div class="evt-br doctor">				
+														<div class="left-events left-img-ph">
+															<?php if($doctor_detail->display_image != ""){ ?>
+															<img src= "<?php echo base_url().'admin/'.$doctor_detail->display_image;?>" >
+															<?php }else{ ?>
+															<img src="<?php echo base_url(); ?>assets/images/home/man.png">
+															<?php  }?>
+														</div>
+													<div class="left-events">
+														<h5>Dr. <?php echo $doctor_detail->doctor_firstname;?> <?php echo $doctor_detail->doctor_lastname;?></h5>
+														<div class="gc-ratting" data-rate="<?php echo $doctor_detail->avg_rating; ?>" ></div> 
+														<div class="pt-ent">
+															<div class="row">
+																<div class="col-lg-1">
+																	<img src="<?php echo base_url(); ?>assets/images/patient-login/13.png" />
+																</div>
+																<div class="col-lg-4">
+																	<h6> <?php if(!empty($doctor_detail->city_name)): ?><?php echo $doctor_detail->city_name;?>,<?php endif; ?><?php if(!empty($doctor_detail->state_name)): ?><?php echo $doctor_detail->state_name;?>, <?php endif; ?><?php echo $doctor_detail->country_name;?> <?php echo $doctor_detail->doctor_office_zip;?></h6>
+																</div>
+															</div>
+														</div>
+													</div>
+													<div class="view-prf">
+														<div class="row">
+															<div class="col-lg-4">
+																<img src="<?php echo base_url(); ?>assets/images/patient-login/14.png" />
+																<h6><a href ="<?php echo base_url(); ?>Doctor/Profile/<?php echo $doctor_detail->id; ?>"><?php if($this->lang->line('doctorfilter_slide_A14')){ ?><?php echo $this->lang->line('doctorfilter_slide_A14'); }else{ ?>View Profile<?php } ?></a></h6>
+															</div>
+															<div class="col-lg-4">
+																<img src="<?php echo base_url(); ?>assets/images/patient-login/15.png" />
+																<h6><a class ="modalbookapp" href ="javascript:void(0);" id="<?php echo $doctor_detail->id; ?>" ><?php if($this->lang->line('doctorfilter_slide_A15')){ ?><?php echo $this->lang->line('doctorfilter_slide_A15'); }else{ ?>Book Online<?php } ?></a></h6>
+															</div>
+														</div>
+													</div>
+												</div>
+												<?php } ?>
+											</div>
+											<div class="col-lg-5 evt-br-1" id="calendar_blk">
+											<?php foreach($doctors as $doctor_detail){ 
+												pull_doccalendar($doctor_detail->id);  } ?>
+											</div>
+											<?php  } } else { ?>						
+												<div class="error"><h1><?php if($this->lang->line('doctorfilter_slide_A17')){ ?><?php echo $this->lang->line('doctorfilter_slide_A17'); }else{ ?>Sorry, No records found. Please try with different keywords.<?php } ?> </h1></div>
+												<div class="clearfix"></div>												
+											<?php  } ?>
+											</div>
+										</div>		
+									</div>
 								</div>
+
 							</div>	
-																					 							 
+							<!----new end- -->
 							<div id="menus2" class="tab-pane fade">
 								<div class="tab-cnt-search">
 									<div class="container">
