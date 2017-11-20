81ce04adf0b569f24a9879b43b2d5327e13468f2
Pilonetto <marlon.pilonetto@gmail.com>
2017-11-20 13:38:56 -0200

    Agendamento médico

--- application/models/Login_Model.php
+++ application/models/Login_Model.php
@@ -30,7 +30,7 @@ class Login_Model extends CI_Model{
 		}
 	}	
 	public function Insert_patient_book($data){	
-		if ($this->db->insert("patient", $data, $data)){
+		if ($this->db->insertex("patient", $data, $data)){
 			return true;
 		}
 	}			
--- system/database/DB_query_builder.php
+++ system/database/DB_query_builder.php
@@ -1626,6 +1626,33 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 		return $this->query($sql);
 	}
 
+	public function insertex($table = '', $set = NULL, $escape = NULL)
+	{
+		if ($set !== NULL)
+		{
+			$this->set($set, '', $escape);
+		}
+
+		if ($this->_validate_insert($table) === FALSE)
+		{   
+			//echo("<script>console.log('insert: validate error');</script>");
+			return FALSE;
+		}
+		echo("<script>console.log('insertex: ".json_encode($set)."');</script>");
+		//echo("<script>console.log('insert: ".json_encode($escape)."');</script>");
+		$sql = $this->_insert(
+			$this->protect_identifiers(
+				$this->qb_from[0], TRUE, $escape, FALSE
+			),
+			array_keys($this->qb_set),
+			array_values($set)
+		);
+		echo("<script>console.log('insertex: ".$sql."');</script>");
+		
+		$this->_reset_write();
+		return $this->query($sql);
+	}
+
 	// --------------------------------------------------------------------
 
 	/**
