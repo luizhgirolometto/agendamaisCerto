16c6420b367223475a240b158e128bdcb8f91158
Pilonetto <marlon.pilonetto@gmail.com>
2017-11-22 10:00:05 -0200

    ajustes calendario

--- application/views/doc_calendar.php
+++ application/views/doc_calendar.php
@@ -1,7 +1,10 @@
 <?php
 	setlocale(LC_ALL, "pt_BR", "pt_BR.iso-8859-1", "pt_BR.utf-8", "portuguese");
 	date_default_timezone_set('America/Sao_Paulo');
+	
   $date= date('D Y-m-d');
+  $date2 = date('D Y-m-d', strtotime($date. ' + 1 days'));
+  $date3 = date('D Y-m-d', strtotime($date. ' + 2 days'));
 ?>
 <div class="evt-br-doc" id="evt-br-doc_<?php echo $id; ?>">
 	<div class="date-head">
@@ -14,10 +17,10 @@
 					<li> <h5><?php echo strftime('%a, %d-%m-%Y', strtotime($data)); ?></h5></li>
 				</div>
 				<div class="dttime-list">
-					<li> <h5><?php echo date('D Y-m-d', strtotime($date. ' + 1 days')) ?></h5> </li>
+					<li> <h5><?php echo strftime('%a, %d-%m-%Y', strtotime($data2)); ?></h5> </li>
 				</div>
 				<div class="dttime-list">	
-					<li> <h5><?php echo date('D Y-m-d', strtotime($date. ' + 2 days')) ?></h5> </li>
+					<li> <h5><?php echo strftime('%a, %d-%m-%Y', strtotime($data3)); ?></h5> </li>
 				</div>
 			</ul>
 		</div>
